30dbfea73aed7bc4047095e27537f91ef5121089
123yogin <parmaryogin04@gmail.com>
2025-03-08 15:03:19 +0530

    update

--- models.py
+++ models.py
@@ -9,9 +9,9 @@ def load_user(user_id):
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128))
+    username = db.Column(db.String(50), nullable=False, unique=True)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    password_hash = db.Column(db.String(255), nullable=False)  # Increase to 255 or more
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
     def set_password(self, password):
