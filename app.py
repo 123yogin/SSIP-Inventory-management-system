0285b3dec9d6282682e831b646efa6d22e9f8f08
123yogin <parmaryogin04@gmail.com>
2025-03-21 17:41:57 +0530

    commit

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_mail import Mail  # Import Flask-Mail
+from flask_mail import Mail  # type: ignore # Import Flask-Mail
 from config import Config
 
 app = Flask(__name__)
